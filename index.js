58db6fa10acd805edf8278192bf05b850e7d2b9b
Alexar <github@alexar.me>
2020-05-03 23:23:17 +1000

    Accesspt the URL as a command line argument.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 
 app.whenReady().then(function() {
+
     w = new BrowserWindow({backgroundColor: '#bf34c2'})
-    console.log('sealant: keepontop')
+
+    const [,, ... args] = process.argv
+    console.log(args)
+    if (args[0]) {
+        console.log('opening '+args[0])
+        w.loadURL(args[0])
+    }
+    if (args[1] == '--top') {
+        console.log('attempting to keep window on top')
+        console.log('sealant: keepontop')
+    }
 })
